706697e01ccbc0d6f1e95ed3d14e248692969668
Gabriel Chaves <gabovvv@gmail.com>
2023-03-15 18:03:32 -0600

    Link to another new page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 // react router imports
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 //library imports
 import { toast } from "react-toastify";
@@ -27,8 +27,8 @@ export async function dashboardAction({ request }) {
   await waait();
 
   const data = await request.formData();
-  const {_action, ...values} = Object.fromEntries(data);
-  if(_action == "newUser"){
+  const { _action, ...values } = Object.fromEntries(data);
+  if (_action == "newUser") {
     try {
       localStorage.setItem("userName", JSON.stringify(values.userName));
       return toast.success(`Bienvenido ${values.userName}`);
@@ -37,36 +37,32 @@ export async function dashboardAction({ request }) {
     }
   }
 
-  if(_action == "createBudget"){
-    try{
+  if (_action == "createBudget") {
+    try {
       //create budget
       createBudget({
         name: values.newBudget,
         amount: values.newBudgetAmount,
-      })
+      });
 
-      return toast.success("Categoría Creada")
-    }
-    catch(e){
-      throw new Error("Hubo un problema al crear tu categoria.")
+      return toast.success("Categoría Creada");
+    } catch (e) {
+      throw new Error("Hubo un problema al crear tu categoria.");
     }
   }
 
-  if(_action == "createExpense"){
-    try{
+  if (_action == "createExpense") {
+    try {
       //create an expense
       createExpense({
-        name:values.newExpense,
-        amount:values.newExpenseAmount,
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
         budgetId: values.newExpenseBudget,
-      })
+      });
 
-      
-
-      return toast.success(`Gasto ${values.newExpense} creado!`)
-    }
-    catch(e){
-      throw new Error("Hubo un problema al crear tu gasto.")
+      return toast.success(`Gasto ${values.newExpense} creado!`);
+    } catch (e) {
+      throw new Error("Hubo un problema al crear tu gasto.");
     }
   }
 }
@@ -75,47 +71,54 @@ const Dashboard = () => {
   return (
     <>
       {userName ? (
-       <div className="dashboard">
-         <h1>
-          Bienvenido, <span className="accent">{userName}</span>
-        </h1>
-        <div className="grid-sm">
-         {
-          budgets && budgets.length > 0
-          ? (
-         <div className="grid-lg">
-            <div className="flex-lg">
-              <AddBudgetForm/>
-              <AddExpenseForm budgets={budgets}/>
-            </div>
-            <h2>Presupuestos actuales</h2>
-            <div className="budgets">
-              {
-                budgets.map((budget)  => (
-                  <BudgetItem key={budget.id} budget={budget} />
-                ))
-              }
-            </div>
-              {
-                expenses && expenses.length > 0 && (
+        <div className="dashboard">
+          <h1>
+            Bienvenido, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
+                </div>
+                <h2>Presupuestos actuales</h2>
+                <div className="budgets">
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
+                </div>
+                {expenses && expenses.length > 0 && (
                   <div className="grid-md">
                     <h2>Gastos Recientes</h2>
-                    <Table  expenses={expenses.sort((a,b) => b.createAt - a.createAt)} />
+                    <Table
+                      expenses={expenses.sort(
+                        (a, b) => b.createAt - a.createAt
+                      )
+                      .slice(0,8)
+                    }
+                    />
+                    {expenses.length > 8 && (
+                      <Link 
+                      to="expenses"
+                      className="btn btn--dark">
+                      Ver todos los gastos
+                      </Link>
+                    )}
                   </div>
-                )
-              }
+                )}
+              </div>
+            ) : (
+              <div className="grid-sm">
+                <p>
+                  Manejar bien tu dinero es el secreto de la libertad financiera
+                </p>
+                <p>Crear un presupuesto para iniciar!</p>
+                <AddBudgetForm />
+              </div>
+            )}
           </div>
-          )
-          :(
-            <div className="grid-sm">
-              <p>Manejar bien tu dinero es el secreto de la libertad financiera</p>
-              <p>Crear un presupuesto para iniciar!</p>
-              <AddBudgetForm/>
-            </div>
-          )
-          }
         </div>
-       </div>
       ) : (
         <Intro />
       )}
